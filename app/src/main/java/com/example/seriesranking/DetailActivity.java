87296ff0b6706b0c8266bcb01f330d323946f9c9
nanguyen162 <nna69543@gmail.com>
2025-05-18 15:56:37 +0700

    UI ranking cho web đọc truyện tranh

---
+++ app/src/main/java/com/example/seriesranking/DetailActivity.java
@@ -0,0 +1,56 @@
+package com.example.seriesranking;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+
+
+public class DetailActivity extends AppCompatActivity {
+
+    Button btnBack;
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ImageView detailImage = findViewById(R.id.detailImage);
+        TextView detailName = findViewById(R.id.detailName);
+        TextView detailCategory = findViewById(R.id.detailCategory);
+        TextView detailLikes = findViewById(R.id.detailLikes);
+        TextView detailRank = findViewById(R.id.detailRank);
+        TextView detailSummary = findViewById(R.id.detailSummary);
+
+        // Lấy dữ liệu từ Intent
+        Intent intent = getIntent();
+        int image = intent.getIntExtra("image", 0);
+        String name = intent.getStringExtra("name");
+        String category = intent.getStringExtra("category");
+        String likes = intent.getStringExtra("likes");
+        int rank = intent.getIntExtra("rank", 0);
+        String summary = intent.getStringExtra("summary");
+
+        // Set dữ liệu lên giao diện
+        detailImage.setImageResource(image);
+        detailName.setText(name);
+        detailCategory.setText(category);
+        detailLikes.setText("Likes: " + likes);
+        detailRank.setText("Rank: #" + rank);
+        detailSummary.setText(summary);
+
+        Button btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/seriesranking/RomanceFragment.java
@@ -0,0 +1,4 @@
+package com.example.seriesranking;
+
+public class RomanceFragment {
+}
