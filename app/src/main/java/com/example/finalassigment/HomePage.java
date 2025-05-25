6f1d1d6885af7d75c8b93b8e9844e1df62e55704
Tran_Hoang_Vu <hoangvudx2004@gmail.com>
2025-05-25 15:33:36 +0700

    update item story 2

--- app/src/main/java/com/example/finalassigment/HomePage.java
+++ app/src/main/java/com/example/finalassigment/HomePage.java
@@ -77,10 +77,10 @@ public class HomePage extends AppCompatActivity {
         rvTrending.setAdapter(trendingAdapter);
 
         RecyclerView rvPopular = findViewById(R.id.rvPopular);
-        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
         rvPopular.setLayoutManager(layoutManager);
         int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.item_spacing);
-        rvPopular.addItemDecoration(new GridSpacingItemDecoration(2, spacingInPixels, false));
+        rvPopular.addItemDecoration(new GridSpacingItemDecoration(3, spacingInPixels, false));
         List<Story> popularStories = new ArrayList<>();
         popularStories.add(new Story(7,R.drawable.my_aggravating_sovereign, "My Aggravating Sovereign", 2676000));
         popularStories.add(new Story(8,R.drawable.webtoon_now, "Heart Acres", 2150000));
@@ -88,7 +88,7 @@ public class HomePage extends AppCompatActivity {
         popularStories.add(new Story(10,R.drawable.monster_eater, "Monster Eater", 1260000));
         popularStories.add(new Story(11,R.drawable.iseops_romance, "Iseop’s Romance", 4500000));
         popularStories.add(new Story(12,R.drawable.my_aggravating_sovereign, "My Aggravating Sovereign", 3200000));
-        StoryAdapter popularAdapter = new StoryAdapter(trendingStories, new StoryAdapter.OnItemClickListener() {
+        StoryAdapter2 popularAdapter = new StoryAdapter2(trendingStories, new StoryAdapter2.OnItemClickListener() {
             @Override
             public void onItemClick(Story story) {
                 // Xử lý click vào item Trending
@@ -154,50 +154,6 @@ public class HomePage extends AppCompatActivity {
             }
         });
     }
-//    private void setupTrending() {
-//        RecyclerView rvTrending = findViewById(R.id.rvTrending);
-//        rvTrending.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-//        List<Story> trendingStories = new ArrayList<>();
-//        trendingStories.add(new Story(1,R.drawable.anhbia1, "SpyX Family", 5590000));
-//        trendingStories.add(new Story(2,R.drawable.anhbia2, "Dr.Stone", 28800000));
-//        trendingStories.add(new Story(3,R.drawable.singles_royale, "Singles Royale", 2500000));
-//        trendingStories.add(new Story(4,R.drawable.dark_mermaid, "Dark Mermaid", 1500000));
-//        trendingStories.add(new Story(5,R.drawable.iseops_romance, "Iseop’s Romance", 4500000));
-//        trendingStories.add(new Story(6,R.drawable.my_aggravating_sovereign, "My Aggravating ", 3200000));
-//        StoryAdapter trendingAdapter = new StoryAdapter(trendingStories);
-//        rvTrending.setAdapter(trendingAdapter);
-//    }
-
-//    private void setupPopular() {
-//        RecyclerView rvPopular = findViewById(R.id.rvPopular);
-//        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
-//        rvPopular.setLayoutManager(layoutManager);
-//
-//        int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.item_spacing);
-//        rvPopular.addItemDecoration(new GridSpacingItemDecoration(2, spacingInPixels, false));
-//
-//        List<Story> trendingStories = new ArrayList<>();
-//
-////        "(7, " + R.drawable.my_aggravating_sovereign + ", 'My Aggravating Sovereign', 'Romance', '267.6K', 'Romantic entanglements in the kingdom.', 7, '+18')," +
-////                "(8, " + R.drawable.press_play_sami + ", 'Press Play, Sami', 'Romance', 'N/A', 'Romantic drama with music.', 8, '0')," +
-////                "(9, " + R.drawable.heart_acres + ", 'Heart Acres', 'Romance', '215K', 'A romantic tale in the countryside.', 9, '-2')," +
-////                "(10, " + R.drawable.the_one_who_parried_death + ", 'The One Who Parried Death', 'Action', '416.8K', 'A warrior defies death itself.', 10, '+7')," +
-////                "(11, " + R.drawable.monster_eater + ", 'Monster Eater', 'Fantasy', '228.6K', 'A hero consumes monsters to gain power.', 11, '-4')," +
-////                "(12, " + R.drawable.nebulas_civilization + ", 'Nebula’s Civilization', 'Fantasy', '336.1K', 'A cosmic civilization-building journey.', 12, '-4')," +
-////                "(13, " + R.drawable.the_witch_and_the_bull + ", 'The Witch and The Bull', 'Fantasy', '8.3M', 'A magical bond between a witch and a bull.', 13, '-8')," +
-////                "(14, " + R.drawable.what_death_taught_me + ", 'What Death Taught Me', 'Romance', '319.9K', 'Lessons of love through loss.', 14, '+11')," +
-//
-//
-//        trendingStories.add(new Story(7,R.drawable.my_aggravating_sovereign, "My Aggravating Sovereign", 2676000));
-//        trendingStories.add(new Story(8,R.drawable.webtoon_now, "Heart Acres", 2150000));
-//        trendingStories.add(new Story(9,R.drawable.heart_acres, "Singles Royale", 2500000));
-//        trendingStories.add(new Story(10,R.drawable.monster_eater, "Monster Eater", 1260000));
-//        trendingStories.add(new Story(11,R.drawable.iseops_romance, "Iseop’s Romance", 4500000));
-//        trendingStories.add(new Story(12,R.drawable.my_aggravating_sovereign, "My Aggravating Sovereign", 3200000));
-//
-//        StoryAdapter popularAdapter = new StoryAdapter(trendingStories);
-//        rvPopular.setAdapter(popularAdapter);
-//    }
 
     private boolean loadFragment(Fragment fragment) {
         if (fragment == null) return false;
---
+++ app/src/main/java/com/example/finalassigment/StoryAdapter2.java
@@ -0,0 +1,75 @@
+package com.example.finalassigment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class StoryAdapter2 extends RecyclerView.Adapter<StoryAdapter2.ViewHolder> {
+
+    private final List<Story> stories;
+    private OnItemClickListener onItemClickListener;
+
+    public StoryAdapter2(List<Story> stories) {
+        this.stories = stories;
+    }
+
+    // Interface tùy chỉnh để xử lý sự kiện click
+    public interface OnItemClickListener {
+        void onItemClick(Story story);
+    }
+
+    // Constructor với listener
+    public StoryAdapter2(List<Story> stories, OnItemClickListener listener) {
+        this.stories = stories;
+        this.onItemClickListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_story2, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Story story = stories.get(position);
+        holder.title.setText(story.getTitle());
+        holder.views.setText(String.format("%dM Views", story.getViews() / 1000000));
+
+        // Tải hình ảnh
+        holder.image.setImageResource(story.getImageRes());
+        // Nếu dùng Glide: Glide.with(holder.itemView.getContext()).load(story.getImageUrl()).into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return Math.min(stories.size(), 6); // Giới hạn tối đa 6 item
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView image;
+        TextView title;
+        TextView views;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.ivStory);
+            title = itemView.findViewById(R.id.tvTitle);
+            views = itemView.findViewById(R.id.tvViews);
+
+            // Xử lý sự kiện click
+            itemView.setOnClickListener(v -> {
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClick(stories.get(getAdapterPosition()));
+                }
+            });
+        }
+    }
+}
